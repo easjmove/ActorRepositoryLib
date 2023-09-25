beb204b06d0ab953b26b5ef01416133ed79b8f6a
move <move@zealand.dk>
2023-09-25 13:03:26 +0200

    added secrets

---
+++ ActorRepositoryLib/Secrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorRepositoryLib
+{
+    public class Secrets
+    {
+        public const string ConnectionString = "your connection string here";
+    }
+}
