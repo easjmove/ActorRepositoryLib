8224b1833a44b214158e381f94cd4122b386b38b
move <move@zealand.dk>
2023-10-03 14:51:59 +0200

    added tostring

--- ActorRepositoryLib/Actor.cs
+++ ActorRepositoryLib/Actor.cs
@@ -31,5 +31,10 @@
             ValidateName();
             ValidateBirthYear();
         }
+
+        public override string ToString()
+        {
+            return $"{Id} : {Name} : {BirthYear}";
+        }
     }
 }
